d120186105f4f6fd3334aa728829c5d7973e4046
Kalkidan Alemu <kali.taye@gmail.com>
2021-12-06 22:08:03 -0500

    add text fields for dialog box

--- src/Components/FormDialog.jsx
+++ src/Components/FormDialog.jsx
@@ -8,6 +8,11 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+import { styled } from "@mui/material/styles";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -38,22 +43,47 @@ export default function FormDialog() {
         <DialogTitle>Add Branch</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
+            Please Add a Branch Name and a Promotion
           </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            label="Email Address"
-            type="email"
+            label="Branch Name"
             fullWidth
             variant="standard"
           />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Longitude"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Latitude"
+            fullWidth
+            variant="standard"
+          />
+          <label htmlFor="contained-button-file">
+            <Input
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+            />
+            <Button variant="contained" component="span">
+              Upload Promotion
+            </Button>
+          </label>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button onClick={handleClose}>Add</Button>
         </DialogActions>
       </Dialog>
     </div>
